6ad7a870417947678a0b2a3c55b5516b0dea024f
Kirill Borisov <kbafintech@gmail.com>
2022-03-05 15:07:00 +0300

    fixed mapping in GunItemComponent

--- ExampleiOS/Source/Classes/UserStrories/AdmiralGun/Models/Plain/GunItemComponent.swift
+++ ExampleiOS/Source/Classes/UserStrories/AdmiralGun/Models/Plain/GunItemComponent.swift
@@ -5,32 +5,11 @@
 //  Created by Borisov Kirill on 05.03.2022.
 //
 
-enum GunItemComponent {
-    case primaryButton
-    case standartTextfield
-    case tab
-    case switcher
-    case toolbar
-    case unknown
-}
-
-extension GunItemComponent {
-
-    static func getComponent(by id: String) -> Self {
-        switch id {
-        case "PrimaryButton":
-            return .primaryButton
-        case "StandartTextfield":
-            return .standartTextfield
-        case "Tab":
-            return .tab
-        case "Switcher":
-            return .switcher
-        case "Toolbar":
-            return .toolbar
-        default:
-            return .unknown
-        }
-    }
-
+enum GunItemComponent: String {
+    case primaryButton = "PrimaryButton"
+    case standartTextfield = "StandartTextfield"
+    case tab = "Tab"
+    case switcher = "Switcher"
+    case toolbar = "Toolbar"
+    case unknown = ""
 }
