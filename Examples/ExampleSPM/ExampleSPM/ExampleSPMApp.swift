2cb6af2b430fb386f517ba59086966bb0a3c62d1
Andrey Dyatkov <andyatkov@gmail.com>
2022-04-19 18:09:57 +0300

    Add SPM example

---
+++ Examples/ExampleSPM/ExampleSPM/ExampleSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSPMApp.swift
+//  ExampleSPM
+//
+//  Created by ANpolyakov on 29.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
