64fb6cc76ce1f9eaf8a4e209e5e4ff38f967d1e2
Алмазов Иван Александрович <meaplasma@gmail.com>
2022-04-25 16:47:31 +0300

    Add to SwiftUI

--- Source/AdmiralSwiftUI/Classes/Components/Controls/SegmentControl/UnderlineSliderTab/UnderlineTab.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Controls/SegmentControl/UnderlineSliderTab/UnderlineTab.swift
@@ -22,9 +22,9 @@ import SwiftUI
  ```
  @State private var isTwoItemControlsState: Int = 0
  UnderlineTab(
-                items: ["One", "Two", "Three"],
-                selection: $isTwoItemControlsState,
-                offset: .constant(16.0))
+ items: ["One", "Two", "Three"],
+ selection: $isTwoItemControlsState,
+ offset: .constant(16.0))
  
  ```
  
@@ -38,12 +38,12 @@ import SwiftUI
  ```
  @State private var isTwoItemControlsState: Int = 0
  UnderlineTab(
-                items: ["One", "Two", "Three"],
-                selection: $isTwoItemControlsState,
-                offset: .constant(16.0))
+ items: ["One", "Two", "Three"],
+ selection: $isTwoItemControlsState,
+ offset: .constant(16.0))
  
  ```
-*/
+ */
 /// A horizontal control with scroll that consists of multiple segments, each segment functioning as a discrete text button.
 @available(iOS 14.0, *)
 public struct UnderlineTab: View {
@@ -59,15 +59,16 @@ public struct UnderlineTab: View {
         static let tabHeight: CGFloat = LayoutGrid.quadrupleModule
         static let spacingElement: CGFloat = LayoutGrid.module
     }
-
+    
     // MARK: - Internal Propeties
-
+    
     @Environment(\.isEnabled) private var isEnabled
-
+    
     // MARK: - Private Propeties
     
     @Binding private var selection: Int
     @Binding private var offset: CGFloat
+    @Binding private var isStaticTabs: Bool
     
     @State private var segmentSize: CGSize = .zero
     @State private var segmentRect: CGRect = .zero
@@ -78,11 +79,11 @@ public struct UnderlineTab: View {
     
     @State private var scheme: UnderlineTabScheme? = nil
     @ObservedObject private var schemeProvider = AppThemeSchemeProvider<UnderlineTabScheme>()
-
+    
     private var activeSegmentView: AnyView {
         let scheme = self.scheme ?? schemeProvider.scheme
         let backgroundColor: Color = ( isEnabled ? scheme.thumbColor.parameter(for: .normal)?.swiftUIColor : scheme.thumbColor.parameter(for: .disabled)?.swiftUIColor) ?? .clear
-
+        
         return RoundedRectangle(cornerRadius: LayoutGrid.halfModule / 4)
             .foregroundColor(backgroundColor)
             .frame(width: segmentRect.size.width, height: LayoutGrid.halfModule / 2)
@@ -90,7 +91,7 @@ public struct UnderlineTab: View {
             .modifier(RectAwareViewModifier(viewRect: $activeSegmentRect))
             .eraseToAnyView()
     }
-
+    
     private let items: [UnderlineTabItem]
     
     // MARK: - Initializer
@@ -99,40 +100,54 @@ public struct UnderlineTab: View {
     public init(
         items: [UnderlineTabItem],
         selection: Binding<Int>,
-        offset: Binding<CGFloat> = .constant(0.0)
+        offset: Binding<CGFloat> = .constant(0.0),
+        isStaticTabs: Binding<Bool> = .constant(false)
     ) {
         self._selection = selection
         self._offset = offset
         self.items = items
+        self._isStaticTabs = isStaticTabs
     }
     
     /// Initializes and returns a newly allocated view object with items.
     public init(
         items: [String],
         selection: Binding<Int>,
-        offset: Binding<CGFloat> = .constant(0.0)
+        offset: Binding<CGFloat> = .constant(0.0),
+        isStaticTabs: Binding<Bool> = .constant(false)
     ) {
-        self.init(items: items.map({ UnderlineTabItem(title: $0, badgeStyle: nil) }), selection: selection, offset: offset)
+        self.init(items: items.map({ UnderlineTabItem(title: $0, badgeStyle: nil) }), selection: selection, offset: offset, isStaticTabs: isStaticTabs)
     }
     
     public var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            ZStack(alignment: .leading) {
-                LazyHStack(spacing: .zero) {
-                    if offset > 0 {
-                        offsetView()
-                    }
-                    ForEach(0..<items.count, id: \.self) { index in
-                        getSegmentView(for: index)
-                    }
-                    if offset > 0 {
-                        offsetView()
-                    }
-                }
-                .frame(height: Constants.tabHeight)
-                .padding(.vertical, Constants.segmentYPadding)
-                activeSegmentView
+        if isStaticTabs {
+            content()
+        } else {
+            ScrollView(.horizontal, showsIndicators: false) {
+                content()
             }
+        }
+    }
+    
+    // MARK: - Private Methods
+    
+    @ViewBuilder
+    private func content() -> some View {
+        ZStack(alignment: .leading) {
+            HStack(alignment: .firstTextBaseline, spacing: .zero) {
+                if offset > 0 {
+                    offsetView()
+                }
+                ForEach(0..<items.count, id: \.self) { index in
+                    getSegmentView(for: index)
+                }
+                if offset > 0 {
+                    offsetView()
+                }
+            }
+            .frame(height: Constants.tabHeight)
+            .padding(.vertical, Constants.segmentYPadding)
+            activeSegmentView
         }
         .edgesIgnoringSafeArea(.all)
         .frame(height: Constants.tabHeight)
@@ -145,8 +160,6 @@ public struct UnderlineTab: View {
             setActiveSegmentX()
         }
     }
-    
-    // MARK: - Private Methods
     
     @ViewBuilder
     private func offsetView() -> some View {
@@ -187,6 +200,7 @@ public struct UnderlineTab: View {
             .lineLimit(1)
             .padding(.vertical, Constants.segmentYPadding)
             .padding(.horizontal, withBadgeStyle ? Constants.segmentBadgeXPadding : Constants.segmentXPadding)
+            .frame(minWidth: 0, maxWidth: isStaticTabs ? .infinity : nil)
             .onTapGesture { onItemTap(index: index) }
             .modifier(RectAwareViewModifier(viewRect: isSelected ? self.$segmentRect : .constant(.zero)))
             .onChange(of: segmentRect, perform: { newValue in
@@ -204,11 +218,19 @@ public struct UnderlineTab: View {
     private func setActiveSegmentX() {
         activeSegmentX = segmentRect.origin.x - scrollViewSegmentRect.origin.x
     }
-
+    
     private func onItemTap(index: Int) {
         guard index < items.count else { return }
         
         selection = index
     }
     
+}
+
+@available(iOS 14.0, *)
+struct UnderlineTabPreviews: PreviewProvider {
+    
+    static var previews: some View {
+        UnderlineTab(items: ["First", "Second"], selection: .constant(0))
+    }
 }
