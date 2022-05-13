76d6a32847971eccc2d0a17a78f90c2fb930c7f8
Алмазов Иван Александрович <meaplasma@gmail.com>
2022-05-13 15:28:20 +0300

    Fix SmallInformer arrow image

--- Source/AdmiralSwiftUI/Classes/Components/Views/Informers/SmallInformer/SmallInformer.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/Informers/SmallInformer/SmallInformer.swift
@@ -13,41 +13,49 @@ import SwiftUI
  SmallInformerArrowDirection - Public enum for direction SmallInformer
  
  SmallInformerArrowDirection can be one of the following values:
- - top - Top 
- - bottom - Cell with ArrowDown Image.
- - empty - Cell without image. There is an indent from the right edge.
- - none - Cell without image. The position of the cell is at the right edge.
+ - top - The arrow is at the top
+ - bottom - The arrow is at the bottom
+ - topRight - The arrow is at the top right
+ - bottomRight - The arrow is at the bottom right
+ 
+ By default SmallInformerArrowDirection is .top
  */
 public enum SmallInformerArrowDirection {
+    /// The arrow is at the top
     case top
+    /// The arrow is at the bottom
     case bottom
+    /// The arrow is at the top right
+    case topRight
+    /// The arrow is at the bottom right
+    case bottomRight
 }
 /**
  SmallInformer - The component is in four status colors with different pop-up positions. Multiline (up to six lines), changes its width depending on the content, the maximum width of the component is 288 px.
-
+ 
  You can create a SmallInformer with the zero frame rectangle by specifying the following parameters in init:
-
+ 
  - title: String - Informer title text.
  - value: InformerStyleSwiftUI - The style of Informer.
  - arrowDirection: SmallInformerArrowDirection - The direction of informers arrow - top or down.
  - arrowOffset: CGFloat - Offest of arrow.
  - cornerRadius: CGFloat - The corner radius of SmallInformer.
-
+ 
  ## Example to create SmallInformer
  # Code
  ```
  SmallInformer(
-     title: .constant("Text"),
-     informerStyle: .default,
-     arrowDirection: .top
+ title: .constant("Text"),
+ informerStyle: .default,
+ arrowDirection: .top
  )
-```
+ ```
  */
 @available(iOS 14.0, *)
 public struct SmallInformer: View {
-
+    
     // MARK: - Constants
-
+    
     private enum Constants {
         static let labelEdgeInsets: EdgeInsets = EdgeInsets(
             top: LayoutGrid.module,
@@ -58,7 +66,7 @@ public struct SmallInformer: View {
         static let topArrowImage = PrivateAsset.Custom.Segment.arrowUp.image
         static let bottomArrowImage = PrivateAsset.Custom.Segment.arrowDown.image
         static let imageSize = CGSize(width: LayoutGrid.doubleModule, height: LayoutGrid.module)
-        static let informerMaxWidth: CGFloat = 243.0
+        static let informerMaxWidth: CGFloat = LayoutGrid.quadrupleModule * 9
     }
     
     // MARK: - Internal Properties
@@ -66,16 +74,16 @@ public struct SmallInformer: View {
     @Environment(\.isEnabled) private var isEnabled
     
     // MARK: - Private Properties
-        
+    
     private let title: String
     private let informerStyle: InformerStyleSwiftUI
     private let arrowDirection: SmallInformerArrowDirection
     private let arrowOffset: CGFloat
     private let cornerRadius: CGFloat
-
+    
     @State private var scheme: SmallInformerScheme? = nil
     @ObservedObject private var schemeProvider = AppThemeSchemeProvider<SmallInformerScheme>()
-
+    
     // MARK: - Initializer
     
     /// Initializes and returns a newly allocated input number object.
@@ -98,14 +106,14 @@ public struct SmallInformer: View {
         self.arrowOffset = arrowOffset
         self.cornerRadius = cornerRadius.rawValue
     }
-
+    
     // MARK: - Layout
-
+    
     public var body: some View {
         let scheme = self.scheme ?? schemeProvider.scheme
         let titleFont = Font(scheme.titleLabelFont.uiFont)
         let titleColor = scheme.titleLabelTextColors.parameter(
-                isEnabled: isEnabled,
+            isEnabled: isEnabled,
             style: informerStyle)?.swiftUIColor
         
         let wrapViewColor = Color(
@@ -113,12 +121,6 @@ public struct SmallInformer: View {
                 isEnabled: isEnabled,
                 style: informerStyle)?.uiColor ?? .clear)
         
-        VStack(alignment: .leading, spacing: .zero) {
-            if arrowDirection == .top {
-                Image(uiImage: Constants.topArrowImage)
-                    .padding(.leading, arrowOffset)
-                    .foregroundColor(wrapViewColor)
-            }
             Text(title)
                 .foregroundColor(titleColor)
                 .font(titleFont)
@@ -127,12 +129,40 @@ public struct SmallInformer: View {
                 .multilineTextAlignment(.leading)
                 .background(wrapViewColor)
                 .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+                .overlay(
+                    Image(uiImage: arrowDirection == .top || arrowDirection == .topRight ? Constants.topArrowImage : Constants.bottomArrowImage)
+                        .offset(y: arrowDirection == .top || arrowDirection == .topRight ? -LayoutGrid.module : LayoutGrid.module)
+                        .padding(arrowDirection == .top || arrowDirection == .bottom ? .leading : .trailing, arrowOffset)
+                        .foregroundColor(wrapViewColor),
+                    alignment: arrowAlignment())
                 .frame(width: Constants.informerMaxWidth, alignment: .leading)
-            if arrowDirection == .bottom {
-                Image(uiImage: Constants.bottomArrowImage)
-                    .padding(.leading, arrowOffset)
-                    .foregroundColor(wrapViewColor)
+    }
+    
+    private func arrowAlignment() -> Alignment {
+        switch arrowDirection {
+        case .top:
+            return .topLeading
+        case .topRight:
+            return .topTrailing
+        case .bottom:
+            return .bottomLeading
+        case .bottomRight:
+            return .bottomTrailing
+        }
+    }
+    
+
+}
+
+@available(iOS 14.0, *)
+struct SmallInformerPreviews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HStack{
+                SmallInformer(title: "Text Informer", informerStyle: .default, arrowDirection: .topRight)
+                Spacer()
             }
+            Spacer()
         }
     }
 }
