9312fb4e5f1665ae89ff92077dd99767e7da8bba
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-07-05 16:20:58 +0300

    test release scripts

--- fastlane/go_scripts/main.go
+++ fastlane/go_scripts/main.go
@@ -39,8 +39,9 @@ func main() {
 		client := auth.GithubClient(os.Args[3], ctx)
 		issues.GetIssues(ctx, os.Getenv("OWNER"), os.Getenv("REPO"), *client)
 	case "createRelease":
-		buildInfo := configureBuildInfo(os.Args[2])
-		release.CreateRelease(ctx, os.Getenv("OWNER"), os.Getenv("REPO"), buildInfo.External_version, os.Args[3])
+		// buildInfo := configureBuildInfo(os.Args[2])
+		externalVersion := "20.0"
+		release.CreateRelease(ctx, os.Getenv("OWNER"), os.Getenv("REPO"), externalVersion, os.Args[3])
 	case "build_failed":
 		buildInfo := configureBuildInfo(os.Args[2])
 		formatedBuildInfoFailed := buildInfo.build_failed_info(os.Args[5])
