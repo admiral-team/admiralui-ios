54cad31596171a23060609d7650bae83f00da3c8
Evgeniy <EMigunov@inno.tech>
2022-08-02 19:08:32 +0300

    Добавил автотесты UI для экранов Themes(падает), Icons

---
+++ ExampleiOS/Tests/AdmiralUIKitTests/AdmiralUIIconsTest.swift
@@ -0,0 +1,59 @@
+//
+//  AdmiralUIKitUITests.swift
+//  AdmiralUIKitUITests
+//
+//  Created by Мигунов Евгений Анатольевич on 01.08.2022.
+//
+
+import XCTest
+
+class AdmiralUIKitIconsTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testIcons() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Icons"]/*[[".cells.staticTexts[\"Icons\"]",".staticTexts[\"Icons\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.staticTexts["Solid"].tap()
+        app.staticTexts["Outline"].tap()
+        
+        app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .collectionView).element.swipeUp()
+        
+        app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .collectionView).element.swipeDown()
+        
+        _ = app.textFields["Поиск"]
+        app.textFields["Поиск"].tap()
+        app.textFields["Поиск"].typeText("Bank")
+
+        app.buttons["closeSolid"].tap()
+                        
+        
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
+
+
---
+++ ExampleiOS/Tests/AdmiralUIKitTests/AdmiralUIThemesTest.swift
@@ -0,0 +1,61 @@
+//
+//  AdmiralUIKitUITests.swift
+//  AdmiralUIKitUITests
+//
+//  Created by Мигунов Евгений Анатольевич on 01.08.2022.
+//
+
+import XCTest
+
+class AdmiralUIKitThemesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testThemes() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Themes"]/*[[".cells.staticTexts[\"Themes\"]",".staticTexts[\"Themes\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Light"]/*[[".cells.staticTexts[\"Light\"]",".staticTexts[\"Light\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.buttons["Применить"].tap()
+        app.tables.staticTexts["Dark"].tap()
+        app.buttons["Применить"].tap()
+        app.tables.staticTexts["SME Light"].tap()
+        app.buttons["Применить"].tap()
+        app.tables.staticTexts["SME Dark"].tap()
+        app.buttons["Применить"].tap()
+        
+        app.buttons["Создать новую тему"].tap()
+        app.staticTexts["Theme one"].tap()
+        app.staticTexts["Theme one"].typeText("New theme")
+                
+        
+        
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
+
+
+
