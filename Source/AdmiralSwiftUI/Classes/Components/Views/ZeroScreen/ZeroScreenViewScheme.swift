15b6d516c3f6de8858fe9a801f56363334d947a4
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-08-30 17:08:02 +0300

    BadgeView ShimmerView ZeroScreen

--- Source/AdmiralSwiftUI/Classes/Components/Views/ZeroScreen/ZeroScreenViewScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/ZeroScreen/ZeroScreenViewScheme.swift
@@ -4,23 +4,53 @@
 //
 //  Created on 05.08.2021.
 //
-
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+ ZeroScreenViewScheme - the visual scheme of BadgeView.
+ You can create a by specifying the following parameters in init:
+ - ZeroScreenViewScheme() - Initialize default ZeroScreenViewScheme with default themezation
+ # Example to create ZeroScreenViewScheme:
+ # Code
+ ```
+ let scheme = ZeroScreenViewScheme()
+ ```
+ */
 public struct ZeroScreenViewScheme: AppThemeScheme {
-    
+
+    // MARK: - Properties
+
+    /// The title color of ZeroScreenView
     public var titleColor: AColor
+
+    /// The title font of ZeroScreenView
     public var titleFont: AFont
-    
+
+    /// The subtitle color of ZeroScreenView
     public var subtitleColor: AColor
+
+    /// The subtitle font of ZeroScreenView
     public var subtitleFont: AFont
-    
+
+    // MARK: - Initializer
+
     public init(theme: AppTheme) {
         titleColor = theme.colors.textPrimary
         titleFont = theme.fonts.title1
-        
+
         subtitleColor = theme.colors.textSecondary
         subtitleFont = theme.fonts.body2
+    }
+
+    public init(
+        titleColor: AColor,
+        titleFont: AFont,
+        subtitleColor: AColor,
+        subtitleFont: AFont
+    ) {
+        self.titleColor = titleColor
+        self.titleFont = titleFont
+        self.subtitleFont = subtitleFont
+        self.subtitleColor = subtitleColor
     }
 }
