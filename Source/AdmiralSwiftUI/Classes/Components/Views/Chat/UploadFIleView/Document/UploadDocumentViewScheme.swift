c374a50b0dc3c0308e57a1a4a8d33e600ab0dc33
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-08-30 15:41:55 +0300

    TextOperationView UploadDocumentView UploadDocumentGrid

--- Source/AdmiralSwiftUI/Classes/Components/Views/Chat/UploadFIleView/Document/UploadDocumentViewScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/Chat/UploadFIleView/Document/UploadDocumentViewScheme.swift
@@ -7,30 +7,81 @@
 
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+  UploadDocumentViewScheme - the visual scheme of UploadDocumentView.
+   You can create a by specifying the following parameters in init:
+   - UploadDocumentViewScheme() - Initialize default UploadDocumentViewScheme with default themezation
+   - UploadDocumentViewScheme(
+         sizeTextFont: AFont,
+         timeTextFont: AFont,
+         messageTextFont: AFont,
+         messageTextColor: AColor,
+         textFont: AFont,
+         backgroundColorLeft: AColor,
+         backgroundColorRight: AColor,
+         nameTextColorRight: AColor,
+         nameTextColorLeft: AColor,
+         timeTextColorRight: AColor,
+         timeTextColorLeft: AColor,
+         iconColorRight: AColor,
+         iconColorLeft: AColor,
+         sizeColorRight: AColor,
+         sizeColorLeft: AColor,
+         circleColorRight: AColor,
+         circleColorLeft: AColor,
+         chevronColorRight: AColor,
+         chevronColorLeft: AColor
+     )
+   # Example to create UploadDocumentViewScheme:
+   # Code
+   ```
+  let scheme = UploadDocumentViewScheme()
+   ```
+*/
 @available(iOS 14.0, *)
 public struct UploadDocumentViewScheme: AppThemeScheme {
 
     // MARK: - Public properties
 
+    /// The font of size text label in UploadDocumentView
     public let sizeTextFont: AFont
+
+    /// The font of time text label in UploadDocumentView
     public let timeTextFont: AFont
 
+    /// The font of message text label in UploadDocumentView
     public var messageTextFont: AFont
+
+    /// The color of message text label in UploadDocumentView
     public var messageTextColor: AColor
 
+    /// The text font of UploadDocumentView
     public let textFont: AFont
+
+    /// The background color of  UploadDocumentView
     public var backgroundColor = UploadDocumentParameters<AColor>()
+
+    /// The name color of  UploadDocumentView
     public var nameTextColor = UploadDocumentParameters<AColor>()
+
+    /// The time  color of  UploadDocumentView
     public var timeTextColor = UploadDocumentParameters<AColor>()
+
+    /// The icon color of  UploadDocumentView
     public var iconColor = UploadDocumentParameters<AColor>()
+
+    /// The size color of  UploadDocumentView
     public var sizeColor = UploadDocumentParameters<AColor>()
+
+    /// The circle color of  UploadDocumentView
     public var circleColor = UploadDocumentParameters<AColor>()
+
+    /// The chevron color of  UploadDocumentView
     public var chevronColor = UploadDocumentParameters<AColor>()
 
     // MARK: - Init/deinit
 
-    public init(theme: AppTheme) {
+    public init(theme: AppTheme = .default) {
         textFont = theme.fonts.body1
         timeTextFont = theme.fonts.caption1
         sizeTextFont = theme.fonts.caption2
@@ -60,6 +111,54 @@ public struct UploadDocumentViewScheme: AppThemeScheme {
         chevronColor.set(parameter: theme.colors.textStaticWhite, style: .right)
     }
 
+    public init(
+        sizeTextFont: AFont,
+        timeTextFont: AFont,
+        messageTextFont: AFont,
+        messageTextColor: AColor,
+        textFont: AFont,
+        backgroundColorLeft: AColor,
+        backgroundColorRight: AColor,
+        nameTextColorRight: AColor,
+        nameTextColorLeft: AColor,
+        timeTextColorRight: AColor,
+        timeTextColorLeft: AColor,
+        iconColorRight: AColor,
+        iconColorLeft: AColor,
+        sizeColorRight: AColor,
+        sizeColorLeft: AColor,
+        circleColorRight: AColor,
+        circleColorLeft: AColor,
+        chevronColorRight: AColor,
+        chevronColorLeft: AColor
+    ) {
+        self.sizeTextFont = sizeTextFont
+        self.timeTextFont = timeTextFont
+        self.messageTextFont = messageTextFont
+        self.messageTextColor = messageTextColor
+        self.textFont = textFont
+        backgroundColor.set(parameter: backgroundColorLeft, style: .left)
+        backgroundColor.set(parameter: backgroundColorRight, style: .right)
+
+        nameTextColor.set(parameter: nameTextColorRight, style: .right)
+        nameTextColor.set(parameter: nameTextColorLeft, style: .left)
+
+        timeTextColor.set(parameter: timeTextColorRight, style: .right)
+        timeTextColor.set(parameter: timeTextColorLeft, style: .left)
+
+        iconColor.set(parameter: iconColorRight, style: .right)
+        iconColor.set(parameter: iconColorLeft, style: .left)
+
+        sizeColor.set(parameter: sizeColorRight, style: .right)
+        sizeColor.set(parameter: sizeColorLeft, style: .left)
+
+        circleColor.set(parameter: circleColorRight, style: .left)
+        circleColor.set(parameter: circleColorLeft, style: .right)
+
+        chevronColor.set(parameter: chevronColorRight, style: .left)
+        chevronColor.set(parameter: chevronColorLeft, style: .right)
+    }
+
 }
 
 @available(iOS 14.0.0, *)
@@ -84,3 +183,4 @@ public struct UploadDocumentParameters<P> {
     }
 
 }
+
