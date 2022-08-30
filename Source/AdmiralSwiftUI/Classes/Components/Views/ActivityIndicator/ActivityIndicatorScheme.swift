342537bb55a7864d0bed622681af40deeddeace8
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-08-30 11:09:58 +0300

    AlertView ActivityIndicator

--- Source/AdmiralSwiftUI/Classes/Components/Views/ActivityIndicator/ActivityIndicatorScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/ActivityIndicator/ActivityIndicatorScheme.swift
@@ -7,20 +7,56 @@
 
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+ ActivityIndicatorScheme - the visual scheme of PlatformButtonStyle.
+  You can create a by specifying the following parameters in init:
+  - ActivityIndicatorScheme() - Initialize default ActivityIndicatorScheme with default themezation
+  - ActivityIndicatorScheme(
+     backgroundColorNormal: AColor,
+     backgroundColorDisabled: AColor,
+     backgroundConstrastNormal: AColor,
+     backgroundConstrastDisabled: AColor
+    )
+  # Example to create ArrowSegmentSliderScheme:
+  # Code
+  ```
+ let scheme = ActivityIndicatorScheme()
+  ```
+*/
 public struct ActivityIndicatorScheme: AppThemeScheme {
-    
+
+    // MARK: - Properties
+
+    /// The background color of ActivityIndicator
     public var backgroundDefaultColor = ControlParameter<AColor>()
+
+    /// The background constant color of ActivityIndicator
     public var backgroundConstrastColor = ControlParameter<AColor>()
-    
-    public init(theme: AppTheme) {
+
+    // MARK: - Initializer
+
+    public init(theme: AppTheme = .default) {
         let alpha = theme.colors.disabledAlpha
-        
+
         backgroundDefaultColor.set(parameter: theme.colors.elementStaticWhite, for: .normal)
         backgroundDefaultColor.set(parameter: theme.colors.elementStaticWhite.withAlpha(alpha), for: .disabled)
-        
+
         backgroundConstrastColor.set(parameter: theme.colors.elementAccent, for: .normal)
         backgroundConstrastColor.set(parameter: theme.colors.elementAccent.withAlpha(alpha), for: .disabled)
     }
-    
+
+    public init(
+        backgroundColorNormal: AColor,
+        backgroundColorDisabled: AColor,
+        backgroundConstrastNormal: AColor,
+        backgroundConstrastDisabled: AColor
+    ) {
+        backgroundDefaultColor.set(parameter: backgroundColorNormal, for: .normal)
+        backgroundDefaultColor.set(parameter: backgroundColorDisabled, for: .disabled)
+
+        backgroundConstrastColor.set(parameter: backgroundConstrastNormal, for: .normal)
+        backgroundConstrastColor.set(parameter: backgroundConstrastDisabled, for: .disabled)
+    }
+
 }
+
--- Source/AdmiralSwiftUI/Classes/Components/Views/Alert/AlertViewScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/Alert/AlertViewScheme.swift
@@ -5,12 +5,13 @@
 //  Created on 10.01.2022.
 //
 
-import Foundation
-
 import AdmiralTheme
 import AdmiralUIResources
+import Foundation
 
 public struct AlertViewScheme: AppThemeScheme {
+
+    // MARK: - Public Properties
 
     /// Styled font of title label that depends on AlertTitleFontStyle.
     public var titleFont = DefaultAlertViewCustomSchemeParameters<AFont, AlertTitleFontStyle>()
@@ -23,17 +24,13 @@ public struct AlertViewScheme: AppThemeScheme {
 
     /// Styled color of message label that depends on AlertColorStyle.
     public var messageColor = DefaultAlertViewCustomSchemeParameters<AColor, AlertColorStyle>()
-    
+
     /// Background color of alert view.
     public var backgroundColor: AColor
 
     // MARK: - Initializer
 
-    init() {
-        self.init(theme: AppTheme.default)
-    }
-    
-    public init(theme: AppTheme) {
+    public init(theme: AppTheme = .default) {
         titleFont.set(parameter: theme.fonts.title1, style: .title1)
         titleFont.set(parameter: theme.fonts.title2, style: .title2)
         titleFont.set(parameter: theme.fonts.subtitle1, style: .subtitle1)
@@ -54,10 +51,10 @@ public struct AlertViewScheme: AppThemeScheme {
         messageColor.set(parameter: theme.colors.textMask, style: .mask)
         messageColor.set(parameter: theme.colors.textPrimary, style: .primary)
         messageColor.set(parameter: theme.colors.textSecondary, style: .secondary)
-        
+
         backgroundColor = theme.colors.backgroundExtraSurface
     }
-    
+
 }
 
 public struct DefaultAlertViewCustomSchemeParameters<P, S: RawRepresentable & Hashable> where S.RawValue == Int {
