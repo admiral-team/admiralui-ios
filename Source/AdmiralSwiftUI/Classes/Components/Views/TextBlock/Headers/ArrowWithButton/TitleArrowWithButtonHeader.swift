53beea6eb9c4e0af92c090e6c763640d4133977e
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-08-30 16:19:40 +0300

    TitleButtonDropDown HeaderStyle TitleArrowWithButtonHeader DropDownHeader TitleHeader

--- Source/AdmiralSwiftUI/Classes/Components/Views/TextBlock/Headers/ArrowWithButton/TitleArrowWithButtonHeader.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/TextBlock/Headers/ArrowWithButton/TitleArrowWithButtonHeader.swift
@@ -8,55 +8,55 @@
 import SwiftUI
 import AdmiralTheme
 import AdmiralUIResources
-
 /**
  TitleArrowWithButtonHeader - A header with header title, arrow and button.
 
  You can create a TitleArrowWithButtonHeader by specifying the following parameters in the initializer
- 
+
  ## Initializer parameters:
- 
+
  - title: String? - Title text block
  - buttonTitle: String - Title button
  - buttonAction: () -> () - The callback action by tapping the button
  - renderingMode: Image.TemplateRenderingMode - Rendering mode for image
  - dropDownHeaderType: DropDownHeaderType? - A type of header. DropDownHeaderType can be: up, down, custom
  - headerStyle: HeaderStyle - A style of header text block. HeaderStyle can be: title, body, headline, headlineSecondary
- 
+
  ## Example to create vertical TitleArrowWithButtonHeader:
  # Code
  ```
-TitleArrowWithButtonHeader(title: "Title", buttonTitle: "Button", buttonAction: {})
-TitleArrowWithButtonHeader(
+ TitleArrowWithButtonHeader(title: "Title", buttonTitle: "Button", buttonAction: {})
+ TitleArrowWithButtonHeader(
  title: "Title",
  buttonTitle: "Button",
  buttonAction: {},
  dropDownHeaderType: .up)
  ```
-*/
+ */
 /// A header with header title, arrow and button.
 @available(iOS 14.0.0, *)
 public struct TitleArrowWithButtonHeader: View {
-    
+
     /// The text that the label displays.
     @Binding public var title: String?
-    
+
     /// The text that the button displays.
     @Binding public var buttonTitle: String
-    
+
     /// Drop down header type.
     @Binding public var dropDownHeaderType: DropDownHeaderType?
-    
+
+    /// The button action
     public var buttonAction: () -> ()
-    
+
     /// Image rendering mode.
     public var renderingMode: Image.TemplateRenderingMode
-    
+
     /// Text block style.
     public var textBlockStyle: TextBlockStyle
-    
+
     // MARK: - Initializer
-    
+
     /// Initializes and returns a newly allocated view object with the zero frame rectangle.
     public init(
         title: String?,
@@ -64,15 +64,18 @@ public struct TitleArrowWithButtonHeader: View {
         buttonAction: @escaping () -> (),
         renderingMode: Image.TemplateRenderingMode = .original,
         dropDownHeaderType: DropDownHeaderType? = nil,
-        headerStyle: HeaderStyle = .title) {
-            self._title = Binding(get: { return title }, set: { _ in })
-            self._buttonTitle = Binding(get: { return buttonTitle }, set: { _ in })
-            self.buttonAction = buttonAction
-            self.renderingMode = renderingMode
-            self.textBlockStyle = headerStyle.textBlockStyle
-            self._dropDownHeaderType = Binding(get: { return dropDownHeaderType }, set: { _ in })
-        }
-    
+        headerStyle: HeaderStyle = .title
+    ) {
+        self._title = Binding(get: { return title }, set: { _ in })
+        self._buttonTitle = Binding(get: { return buttonTitle }, set: { _ in })
+        self.buttonAction = buttonAction
+        self.renderingMode = renderingMode
+        self.textBlockStyle = headerStyle.textBlockStyle
+        self._dropDownHeaderType = Binding(get: { return dropDownHeaderType }, set: { _ in })
+    }
+
+    // MARK: - Body
+
     public var body: some View {
         let titleWithImageListStyle = TitleWithImageListStyle(rawValue: textBlockStyle.rawValue)
         return ListCell(
@@ -83,7 +86,7 @@ public struct TitleArrowWithButtonHeader: View {
                 titleWithImageListStyle: titleWithImageListStyle)
             },
             trailingView: { ButtonListView(text: buttonTitle, action: buttonAction) })
-            .configCell(minHeight: textBlockStyle.minHeight, edgeInsets: textBlockStyle.edgeInsets)
+        .configCell(minHeight: textBlockStyle.minHeight, edgeInsets: textBlockStyle.edgeInsets)
     }
 }
 
