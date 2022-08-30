88ba8d553f16a12b01da9d6a7996485c7876645c
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-08-30 09:12:53 +0300

    ChatButton GhostButton

--- Source/AdmiralSwiftUI/Classes/Components/Controls/Buttons/ChatButton/ChatButtonScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Controls/Buttons/ChatButton/ChatButtonScheme.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+ ChatButtonScheme - the visual scheme of ChatButton.
+ You can create a by specifying the following parameters in init:
+ - ChatButtonScheme() - Initialize default ChatButtonScheme with default themezation
+ - ChatButtonScheme(
+     buttonColorHighlighted: AColor,
+     buttonColorNormal: AColor,
+     buttonColorSelected: AColor,
+     buttonColorDisabled: AColor,
+     sendButtonColor: AColor
+   )
+ # Example to create ChatButtonScheme:
+ # Code
+ ```
+let scheme = ChatButtonScheme()
+ ```
+ */
 public struct ChatButtonScheme: AppThemeScheme {
 
+    // MARK: - Public Properties
+
+    /// The button color of ChatButton
     public var buttonColor = ControlParameter<AColor>()
+
+    /// The send button color of ChatButton
     public var sendButtonColor: AColor
 
-    public init(theme: AppTheme) {
+    // MARK: - Initializer
+
+    public init(theme: AppTheme = .default) {
         let alpha = theme.colors.disabledAlpha
         buttonColor.set(parameter: theme.colors.backgroundAccent, for: .highlighted)
         buttonColor.set(parameter: theme.colors.backgroundAccent, for: .normal)
@@ -23,4 +46,19 @@ public struct ChatButtonScheme: AppThemeScheme {
         sendButtonColor = theme.colors.elementStaticWhite
     }
 
+    public init(
+        buttonColorHighlighted: AColor,
+        buttonColorNormal: AColor,
+        buttonColorSelected: AColor,
+        buttonColorDisabled: AColor,
+        sendButtonColor: AColor
+    ) {
+        buttonColor.set(parameter: buttonColorHighlighted, for: .highlighted)
+        buttonColor.set(parameter: buttonColorNormal, for: .normal)
+        buttonColor.set(parameter: buttonColorSelected, for: .selected)
+        buttonColor.set(parameter: buttonColorDisabled, for: .disabled)
+        self.sendButtonColor = sendButtonColor
+    }
+
 }
+
--- Source/AdmiralSwiftUI/Classes/Components/Controls/Buttons/GhostButton/GhostButtonScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Controls/Buttons/GhostButton/GhostButtonScheme.swift
@@ -8,20 +8,54 @@
 import SwiftUI
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+ GhostButtonScheme - the visual scheme of PageControlView.
+ You can create a by specifying the following parameters in init:
+ - GhostButtonScheme() - Initialize default GhostButtonScheme with default themezation
+ - GhostButtonScheme(
+     font: AFont,
+     textColorNormal: AColor,
+     textColorHighlighted: AColor,
+     textColorDisabled: AColor
+   )
+ # Example to create GhostButtonScheme:
+ # Code
+ ```
+let scheme = GhostButtonScheme()
+ ```
+ */
 @available(iOS 14.0.0, *)
 public struct GhostButtonScheme: AppThemeScheme {
-    
+
+    // MARK: - Public Properties
+
+    /// The font color of GhostButton
     public let font: AFont
+
+    /// The text color GhostButton
     public var textColor = ControlParameter<AColor>()
-    
-    public init(theme: AppTheme) {
+
+    // MARK: - Initializer
+
+    public init(theme: AppTheme = .default) {
         let alpha = theme.colors.disabledAlpha
-        
+
         font = theme.fonts.body1
-        
+
         textColor.set(parameter: theme.colors.textAccent, for: .normal)
         textColor.set(parameter: theme.colors.textAccentPressed, for: .highlighted)
         textColor.set(parameter: theme.colors.textAccent.withAlpha(alpha), for: .disabled)
+    }
+
+    public init(
+        font: AFont,
+        textColorNormal: AColor,
+        textColorHighlighted: AColor,
+        textColorDisabled: AColor
+    ) {
+        self.font = font
+        textColor.set(parameter: textColorNormal, for: .normal)
+        textColor.set(parameter: textColorHighlighted, for: .highlighted)
+        textColor.set(parameter: textColorDisabled, for: .disabled)
     }
 }
