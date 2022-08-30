a56c1e2cc48fa56c8cff982397c290a1e947a1fe
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-08-30 11:41:06 +0300

    ChatBubbleView

--- Source/AdmiralSwiftUI/Classes/Components/Views/Chat/ChatBubbleView/ChatBubbleStatusView.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/Chat/ChatBubbleView/ChatBubbleStatusView.swift
@@ -8,7 +8,6 @@
 import AdmiralTheme
 import AdmiralUIResources
 import SwiftUI
-
 /// The style of ChatBubbleStatusView.
 public enum ChatBubbleStatusStyle: Int {
     /// The default style of ChatBubbleStatusView.
@@ -20,15 +19,15 @@ public enum ChatBubbleStatusStyle: Int {
  ChatBubbleStatusView - A view for chat message with time.
 
  You can create a ChatBubbleStatusView by specifying the following parameters in the initializer
- 
+
  ## Initializer parameters:
- 
+
  - time: String - Time text message
  - status: ChatStatus - A status ChatBubbleView. Can be in the following states: loading, error, sent, receive, read
  - direction: ChatDirection - A direction for text mesage. Can be: left, right
  - time: String - Time text message.
  - style: ChatBubbleStatusStyle - The style ChatBubbleStatusView
- 
+
  ## Example to create ChatBubbleStatusView:
  # Code
  ```
@@ -40,37 +39,44 @@ public enum ChatBubbleStatusStyle: Int {
 */
 @available(iOS 14.0, *)
 public struct ChatBubbleStatusView: View {
-    
+
     // MARK: - Public Properties
-    
+
+    /// The time of ChatBubbleStatusView
     @State public var time: String
-    
+
+    /// The status of ChatBubbleStatusView
     @State public var status: ChatStatus?
-    
+
+    /// The direction of ChatBubbleStatusView
     @State public var direction: ChatDirection
 
+    /// The style of ChatBubbleStatusView
     @State public var style: ChatBubbleStatusStyle
 
-    
     // MARK: - Private Properties
-    
-    @State private var scheme: ChatBubbleStatusViewScheme? = nil
+
+    @Binding private var scheme: ChatBubbleStatusViewScheme?
     @ObservedObject var schemeProvider = AppThemeSchemeProvider<ChatBubbleStatusViewScheme>()
-    
+
     // MARK: - Initializer
 
     public init(
         time: String,
         status: ChatStatus? = nil,
         direction: ChatDirection,
-        style: ChatBubbleStatusStyle = .default
+        style: ChatBubbleStatusStyle = .default,
+        scheme: Binding<ChatBubbleStatusViewScheme?> = .constant(nil)
     ) {
         self._time = .init(initialValue: time)
         self._status = .init(initialValue: status)
         self._direction = .init(initialValue: direction)
         self._style = .init(initialValue: style)
+        self._scheme = scheme
     }
-    
+
+    // MARK: - Body
+
     public var body: some View {
         let scheme = self.scheme ?? schemeProvider.scheme
         var image: Image?
@@ -99,12 +105,12 @@ public struct ChatBubbleStatusView: View {
         }
         .fixedSize()
     }
-    
+
 }
 
 @available(iOS 14.0, *)
 struct ChatBubbleStatusView_Previews: PreviewProvider {
-    
+
     static var previews: some View {
         VStack {
             ChatBubbleStatusView(time: "12:34", status: .receive, direction: .left)
--- Source/AdmiralSwiftUI/Classes/Components/Views/Chat/ChatBubbleView/ChatBubbleViewScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/Chat/ChatBubbleView/ChatBubbleViewScheme.swift
@@ -7,51 +7,101 @@
 
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+ ChatBubbleViewScheme - the visual scheme of ChatBubbleView.
+  You can create a by specifying the following parameters in init:
+  - ChatBubbleViewScheme() - Initialize default StandardTabScheme with default themezation
+  - ChatBubbleViewScheme(
+     textFont: AFont,
+     textColorLeft: AColor,
+     textColorRight: AColor,
+     backgroundColorLeft: AColor,
+     backgroundColorRight: AColor,
+     nameTextFont: AFont,
+     nameTextColor: AColor
+    )
+  # Example to create ChatBubbleViewScheme:
+  # Code
+  ```
+ let scheme = ChatBubbleViewScheme()
+  ```
+  */
 @available(iOS 14.0, *)
 public struct ChatBubbleViewScheme: AppThemeScheme {
-    
+
+    // MARK: - Properties
+
+    /// The text font of ChatBubbleView
     public var textFont: AFont
+
+    /// The text color of ChatBubbleView
     public var textColor = ChatBubbleViewParameters<AColor>()
+
+    /// The background color of  ChatBubbleView
     public var backgroundColor = ChatBubbleViewParameters<AColor>()
-    
+
+    /// The font of name text label in ChatBubbleView
     public var nameTextFont: AFont
+
+    /// The color of name text label in ChatBubbleView
     public var nameTextColor: AColor
-    
+
+    // MARK: - Initializer
+
     public init(theme: AppTheme) {
         textColor.set(parameter: theme.colors.textPrimary, style: .left)
         textColor.set(parameter: theme.colors.textStaticWhite, style: .right)
 
         backgroundColor.set(parameter: theme.colors.backgroundAdditionalOne, style: .left)
         backgroundColor.set(parameter: theme.colors.backgroundAccent, style: .right)
-        
+
         textFont = theme.fonts.body1
-        
+
         nameTextFont = theme.fonts.caption1
         nameTextColor = theme.colors.textSecondary
     }
-    
+
+    public init(
+        textFont: AFont,
+        textColorLeft: AColor,
+        textColorRight: AColor,
+        backgroundColorLeft: AColor,
+        backgroundColorRight: AColor,
+        nameTextFont: AFont,
+        nameTextColor: AColor
+    ) {
+        textColor.set(parameter: textColorLeft, style: .left)
+        textColor.set(parameter: textColorRight, style: .right)
+
+        backgroundColor.set(parameter: backgroundColorLeft, style: .left)
+        backgroundColor.set(parameter: backgroundColorRight, style: .right)
+
+        self.textFont = textFont
+        self.nameTextFont = nameTextFont
+        self.nameTextColor = nameTextColor
+    }
+
 }
 
 @available(iOS 14.0, *)
 public struct ChatBubbleViewParameters<P> {
     public var parameters: [String: P?] = [:]
-    
+
     public mutating func set(parameter: P?, style: ChatDirection) {
         let key = paramKey(style: style)
         parameters[key] = parameter
     }
-    
+
     public func parameter(for style: ChatDirection) -> P? {
         let key = paramKey(style: style)
         let defaultKey = paramKey(style: style)
-        
+
         guard let parameter = parameters[key] ?? parameters[defaultKey] else { return nil }
         return parameter
     }
-    
+
     private func paramKey(style: ChatDirection) -> String {
         return "\(style.rawValue)"
     }
-    
+
 }
