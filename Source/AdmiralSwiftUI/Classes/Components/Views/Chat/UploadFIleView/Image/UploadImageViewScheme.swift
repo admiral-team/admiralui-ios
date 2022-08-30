57c4bd0a541822bde0a2a215f493faeeb28f4269
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-08-30 15:49:22 +0300

    Uploading Image View

--- Source/AdmiralSwiftUI/Classes/Components/Views/Chat/UploadFIleView/Image/UploadImageViewScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/Chat/UploadFIleView/Image/UploadImageViewScheme.swift
@@ -7,14 +7,33 @@
 
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+  UploadImageViewScheme - the visual scheme of UploadImageView.
+   You can create a by specifying the following parameters in init:
+   - UploadImageViewScheme() - Initialize default UploadImageViewScheme with default themezation
+   - UploadImageViewScheme(
+     textColor: AColor,
+     textFont: AFont,
+     loadingMaskColor: AColor
+     )
+   # Example to create UploadImageViewScheme:
+   # Code
+   ```
+  let scheme = UploadImageViewScheme()
+   ```
+*/
 @available(iOS 14.0, *)
 public struct UploadImageViewScheme: AppThemeScheme {
 
     // MARK: - Properties
 
+    /// The text color of UploadImageView
     public var textColor: AColor
+
+    /// The text font of UploadImageView
     public var textFont: AFont
+
+    /// The loading mask color of UploadImageView
     public var loadingMaskColor: AColor
 
     // MARK: - Init/deinit
@@ -25,4 +44,15 @@ public struct UploadImageViewScheme: AppThemeScheme {
         textFont = theme.fonts.caption1
     }
 
+    public init(
+        textColor: AColor,
+        textFont: AFont,
+        loadingMaskColor: AColor
+    ) {
+        self.textFont = textFont
+        self.textColor = textColor
+        self.loadingMaskColor = loadingMaskColor
+    }
+
 }
+
