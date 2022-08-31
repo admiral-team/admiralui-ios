0477256244039dbbf157d9d1423c4c1bef4b3b7d
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-08-31 12:53:34 +0300

    CalendarWeekView

--- Source/AdmiralSwiftUI/Classes/Components/Views/Calendar/CalendarWeekView/CalendarWeekViewScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/Calendar/CalendarWeekView/CalendarWeekViewScheme.swift
@@ -5,22 +5,55 @@
 //  Created on 09.06.2021.
 //
 
-import SwiftUI
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+ CalendarWeekViewScheme - the visual scheme of CalendarWeekView.
+ You can create a by specifying the following parameters in init:
+ - CalendarWeekViewScheme() - Initialize default CalendarWeekViewScheme with default themezation
+ - CalendarWeekViewScheme(
+ titleLabelColor: AColor,
+ backgroundColor: AColor,
+ titleLabelFont: AFont
+ )
+ # Example to create CalendarWeekViewScheme:
+ # Code
+ ```
+ let scheme = CalendarWeekViewScheme()
+ ```
+ */
 @available(iOS 14.0.0, *)
 public struct CalendarWeekViewScheme: AppThemeScheme {
-    
+
+    // MARK: - Properties
+
+    /// The title label color of CalendarWeekView
     public var titleLabelColor: AColor
+
+    /// The background of CalendarWeekView
     public var backgroundColor: AColor
+
+    /// The title label font of CalendarWeekView
     public var titleLabelFont: AFont
-    
+
+    // MARK: - Initializer
+
     public init(theme: AppTheme) {
         titleLabelFont = theme.fonts.subhead2
-        
+
         backgroundColor = theme.colors.backgroundBasic
         titleLabelColor = theme.colors.textSecondary
     }
-    
+
+    public init(
+        titleLabelColor: AColor,
+        backgroundColor: AColor,
+        titleLabelFont: AFont
+    ) {
+        self.titleLabelFont = titleLabelFont
+
+        self.backgroundColor = backgroundColor
+        self.titleLabelColor = titleLabelColor
+    }
+
 }
