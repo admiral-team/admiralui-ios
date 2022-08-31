0e041c6f6b615155ab2d0479a78ec230d2c0e1f7
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-08-31 13:04:14 +0300

    MonthYearView UIKitPickerView

--- Source/AdmiralSwiftUI/Classes/Components/Views/Calendar/MonthYearView/MonthYearViewScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/Calendar/MonthYearView/MonthYearViewScheme.swift
@@ -4,23 +4,55 @@
 //
 //  Created on 09.06.2021.
 //
-
-import SwiftUI
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+ MonthYearViewScheme - the visual scheme of MonthYearView.
+ You can create a by specifying the following parameters in init:
+ - MonthYearViewScheme() - Initialize default MonthYearViewScheme with default themezation
+ - MonthYearViewScheme(
+ titleLabelColor: AColor,
+ backgroundColor: AColor,
+ titleLabelFont: AFont
+ )
+ # Example to create MonthYearViewScheme:
+ # Code
+ ```
+ let scheme = MonthYearViewScheme()
+ ```
+ */
 @available(iOS 14.0.0, *)
-public struct MonthYearViewScheme: AppThemeScheme {
-    
-    public var titleLabelColor: AColor
-    public var backgroundColor: AColor
-    public var titleLabelFont: AFont
-    
-    public init(theme: AppTheme) {
+struct MonthYearViewScheme: AppThemeScheme {
+
+    // MARK: - Properties
+
+    /// The title label color of MonthYearView
+    var titleLabelColor: AColor
+
+    /// The background color of MonthYearView
+    var backgroundColor: AColor
+
+    /// The title label font of MonthYearView
+    var titleLabelFont: AFont
+
+    // MARK: - Initializer
+
+    init(theme: AppTheme = .default) {
         titleLabelFont = theme.fonts.subtitle1
-        
+
         backgroundColor = theme.colors.backgroundBasic
         titleLabelColor = theme.colors.textPrimary
     }
-    
+
+    init(
+        titleLabelColor: AColor,
+        backgroundColor: AColor,
+        titleLabelFont: AFont
+    ) {
+        self.titleLabelFont = titleLabelFont
+
+        self.backgroundColor = backgroundColor
+        self.titleLabelColor = titleLabelColor
+    }
+
 }
--- Source/AdmiralSwiftUI/Classes/Components/Views/Calendar/UIKitPickerView/UIKitPickerViewScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Views/Calendar/UIKitPickerView/UIKitPickerViewScheme.swift
@@ -4,21 +4,47 @@
 //
 //  Created on 14.07.2021.
 //
-
-import SwiftUI
 import AdmiralTheme
 import AdmiralUIResources
-
+/**
+ PickerViewScheme - the visual scheme of UIKitPickerView.
+ You can create a by specifying the following parameters in init:
+ - PickerViewScheme() - Initialize default PickerViewScheme with default themezation
+ - PickerViewScheme(
+ backgroundColor: AColor,
+ titleColor: AColor
+ )
+ # Example to create PickerViewScheme:
+ # Code
+ ```
+ let scheme = PickerViewScheme()
+ ```
+ */
 @available(iOS 14.0.0, *)
 public struct PickerViewScheme: AppThemeScheme {
-    
+
+    // MARK: - Properties
+
+    /// The background color of PickerView
     public var backgroundColor: AColor
+
+    /// The title color of PickerView
     public var titleColor: AColor
-    
-    public init(theme: AppTheme) {
+
+    // MARK: - Initializer
+
+    public init(theme: AppTheme = .default) {
         titleColor = theme.colors.textPrimary
         backgroundColor = theme.colors.backgroundBasic
     }
-    
+
+    public init(
+        backgroundColor: AColor,
+        titleColor: AColor
+    ) {
+        self.backgroundColor = backgroundColor
+        self.titleColor = titleColor
+    }
+
 }
 
