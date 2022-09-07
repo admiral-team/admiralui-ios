faaa2dea43cb0e91b0702f920540a000c76ba2b2
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-09-07 13:18:56 +0300

    code review

--- Source/AdmiralSwiftUI/Classes/Components/Controls/Switch/CustomSwitchScheme.swift
+++ Source/AdmiralSwiftUI/Classes/Components/Controls/Switch/CustomSwitchScheme.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import AdmiralTheme
 import AdmiralUIResources
 /**
- CustomSwitchScheme - the visual scheme of CustomSwitch.
+ CustomSwitchScheme - the visual scheme.
  You can create a by specifying the following parameters in init:
  - CustomSwitchScheme() - Initialize default CustomSwitchScheme with default themezation
  # Example to create CustomSwitchScheme:
@@ -23,13 +23,13 @@ public struct CustomSwitchScheme: AppThemeScheme  {
 
     // MARK: - Properties
 
-    /// The font of CustomSwitch
+    /// The font
     public var font: AFont
 
-    /// The tint color of CustomSwitch
+    /// The tint color
     public var tintColor: AColor
 
-    /// The text color of CustomSwitch
+    /// The text color
     public var textColor: AColor
 
     // MARK: - Initializer
