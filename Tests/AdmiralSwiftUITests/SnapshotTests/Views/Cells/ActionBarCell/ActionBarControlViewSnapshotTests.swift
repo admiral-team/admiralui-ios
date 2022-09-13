9e5e01826eace23d8b1363e2cdbd9727d6fbb695
Andrey Dyatkov <andyatkov@gmail.com>
2022-09-13 14:31:41 +0300

    Fix swiftui test action bar control

--- Tests/AdmiralSwiftUITests/SnapshotTests/Views/Cells/ActionBarCell/ActionBarControlViewSnapshotTests.swift
+++ Tests/AdmiralSwiftUITests/SnapshotTests/Views/Cells/ActionBarCell/ActionBarControlViewSnapshotTests.swift
@@ -53,7 +53,7 @@ final class ActionBarControlViewSnapshotTests: XCTestCase {
         let view = ActionBarControlView(
             image: Image(uiImage: Asset.Category.Outline.addCommentOutline.image),
             imageStyle: .accent,
-            scheme: ActionBarControlScheme(theme: SwiftUIThemeManager.shared.theme),
+            schemeProvider: .constant(scheme: ActionBarControlScheme(theme: SwiftUIThemeManager.shared.theme)),
             tapActionBar: {}
         )
         return view
