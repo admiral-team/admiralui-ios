abc8c80362708c722babbd9a33d4f4dceeefa1df
Evgeniy <EMigunov@inno.tech>
2022-10-12 16:23:11 +0300

    Add UI Test for Currency

---
+++ ExampleiOS/Tests/AdmiralUIKitTests/AddUIKitCurrencyTest.swift
@@ -0,0 +1,84 @@
+//
+//  AddUIKitCurrencyTest.swift
+//  AdmiralUIKitTests
+//
+
+import XCTest
+
+class AddUIKitCurrencyTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testCurrencyDefault() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.tables.staticTexts["Currency"].tap()
+        app.tables.staticTexts["Default"].tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.staticTexts["Light"].tap()
+        app.staticTexts["Dark"].tap()
+        app.staticTexts["SME Light"].tap()
+        app.staticTexts["SME Dark"].tap()
+        
+        app.buttons["Ellipse"].tap()
+    }
+    
+    func testCurrencyIcons() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.tables.staticTexts["Currency"].tap()
+        app.tables.staticTexts["Icons"].tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.staticTexts["Light"].tap()
+        app.staticTexts["Dark"].tap()
+        app.staticTexts["SME Light"].tap()
+        app.staticTexts["SME Dark"].tap()
+        
+        app.buttons["Ellipse"].tap()
+    }
+    
+    func testCurrencyFlags() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.tables.staticTexts["Currency"].tap()
+        app.tables.staticTexts["Flags"].tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.staticTexts["Light"].tap()
+        app.staticTexts["Dark"].tap()
+        app.staticTexts["SME Light"].tap()
+        app.staticTexts["SME Dark"].tap()
+        
+        app.buttons["Ellipse"].tap()
+    }
+    
+    func testCurrencyIconsFlags() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.tables.staticTexts["Currency"].tap()
+        app.tables.staticTexts["Icons & Flags"].tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.staticTexts["Light"].tap()
+        app.staticTexts["Dark"].tap()
+        app.staticTexts["SME Light"].tap()
+        app.staticTexts["SME Dark"].tap()
+        
+        app.buttons["Ellipse"].tap()
+    }
+}
