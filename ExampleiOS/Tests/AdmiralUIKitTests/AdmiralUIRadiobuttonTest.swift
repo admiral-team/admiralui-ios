e7381cab197893df0236a1360ea6f8f2a1284617
Evgeniy <EMigunov@inno.tech>
2022-11-29 13:34:45 +0300

    Add UI test for Radiobutton with accessibility

--- ExampleiOS/Tests/AdmiralUIKitTests/AdmiralUIRadiobuttonTest.swift
+++ ExampleiOS/Tests/AdmiralUIKitTests/AdmiralUIRadiobuttonTest.swift
@@ -34,10 +34,10 @@ class AdmiralUIRadiobuttonTest: XCTestCase {
         
         app.buttons["Ellipse"].tap()
         
-        app.staticTexts["Light"].tap()
-        app.staticTexts["Dark"].tap()
-        app.staticTexts["SME Light"].tap()
-        app.staticTexts["SME Dark"].tap()
+        app.otherElements.matching(identifier: "ThemeSwitchButton_Light").element.tap()
+        app.otherElements.matching(identifier: "ThemeSwitchButton_Dark").element.tap()
+        app.otherElements.matching(identifier: "ThemeSwitchButton_SME Light").element.tap()
+        app.otherElements.matching(identifier: "ThemeSwitchButton_SME Dark").element.tap()
         
         app.buttons["Ellipse"].tap()
     }
