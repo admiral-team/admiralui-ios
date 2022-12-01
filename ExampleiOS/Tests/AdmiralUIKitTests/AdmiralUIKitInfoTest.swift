19a6740308eaa70ae93a3ed2a4cf7adaea46bf40
Evgeniy <EMigunov@inno.tech>
2022-12-01 15:18:06 +0300

    Add UI test for Info

--- ExampleiOS/Tests/AdmiralUIKitTests/AdmiralUIKitInfoTest.swift
+++ ExampleiOS/Tests/AdmiralUIKitTests/AdmiralUIKitInfoTest.swift
@@ -20,7 +20,7 @@ class AdmiralUIKitInfoTest: XCTestCase {
         
         app.buttons["Ellipse"].tap()
         
-        app.staticTexts["Light"].tap()
+        app.otherElements.matching(identifier: "ThemeSwitchButton_Light").element.tap()
         
         app.buttons["Ellipse"].tap()
         
@@ -34,7 +34,35 @@ class AdmiralUIKitInfoTest: XCTestCase {
         
         app.buttons["Ellipse"].tap()
         
-        app.staticTexts["Dark"].tap()
+        app.otherElements.matching(identifier: "ThemeSwitchButton_Dark").element.tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["Инфо"].tap()
+        app.tables.staticTexts["Инфо"].tap()
+    }
+    
+    func testInfoSMELight() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.otherElements.matching(identifier: "ThemeSwitchButton_SME Light").element.tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["Инфо"].tap()
+        app.tables.staticTexts["Инфо"].tap()
+    }
+    
+    func testInfoSMEDark() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.otherElements.matching(identifier: "ThemeSwitchButton_SME Dark").element.tap()
         
         app.buttons["Ellipse"].tap()
         
