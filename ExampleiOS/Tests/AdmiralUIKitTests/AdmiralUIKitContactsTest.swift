19698cf300d72e57d45c07eed63b7499898fc402
Evgeniy <EMigunov@inno.tech>
2022-12-01 15:27:38 +0300

    Add UI test for Contacts

--- ExampleiOS/Tests/AdmiralUIKitTests/AdmiralUIKitContactsTest.swift
+++ ExampleiOS/Tests/AdmiralUIKitTests/AdmiralUIKitContactsTest.swift
@@ -20,7 +20,7 @@ class AdmiralUIKitContactsTest: XCTestCase {
         
         app.buttons["Ellipse"].tap()
         
-        app.staticTexts["Light"].tap()
+        app.otherElements.matching(identifier: "ThemeSwitchButton_Light").element.tap()
         
         app.buttons["Ellipse"].tap()
         
@@ -35,7 +35,37 @@ class AdmiralUIKitContactsTest: XCTestCase {
         
         app.buttons["Ellipse"].tap()
         
-        app.staticTexts["Dark"].tap()
+        app.otherElements.matching(identifier: "ThemeSwitchButton_Dark").element.tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["Инфо"].tap()
+        app.tables.staticTexts["Контакты"].tap()
+        app.staticTexts["Телеграмм канал"].tap()
+    }
+    
+    func testContactsSMELight() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.otherElements.matching(identifier: "ThemeSwitchButton_SME Light").element.tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["Инфо"].tap()
+        app.tables.staticTexts["Контакты"].tap()
+        app.staticTexts["Телеграмм канал"].tap()
+    }
+    
+    func testContactsSMEDark() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.otherElements.matching(identifier: "ThemeSwitchButton_SME Dark").element.tap()
         
         app.buttons["Ellipse"].tap()
         
