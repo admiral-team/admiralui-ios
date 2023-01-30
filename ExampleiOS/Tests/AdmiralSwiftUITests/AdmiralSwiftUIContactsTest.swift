d59a8ee94bc53d7531f0b959ce4d342696f44b58
Evgeniy <EMigunov@inno.tech>
2023-01-30 17:37:33 +0300

    Add UI test for SwiftUI for (Alert, Contacts, FAQ, Currency, Info)

---
+++ ExampleiOS/Tests/AdmiralSwiftUITests/AdmiralSwiftUIContactsTest.swift
@@ -0,0 +1,72 @@
+//
+//  AdmiralSwiftUIContactsTest.swift
+//  AdmiralSwiftUITests
+//
+
+import XCTest
+
+class AdmiralSwiftUIContactsTest: XCTestCase {
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+    
+    override func tearDownWithError() throws {
+    }
+    
+    func testCheckSwiftUI() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["SeatingTabBarItem"].tap()
+        let swiftUISwitcher = app.switches["SwitchToSwiftUI"]
+        
+        if swiftUISwitcher.value as? String == "0" {
+            swiftUISwitcher.tap()
+        }
+    }
+    
+    func testDarkContact() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        app.buttons["Dark"].tap()
+        app.buttons["InfoTabBarItem"].tap()
+        app.tables.staticTexts["Контакты"].tap()
+        app.staticTexts["Телеграмм канал"].tap()
+    }
+    
+    func testLightContact() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        app.buttons["Light"].tap()
+        app.buttons["InfoTabBarItem"].tap()
+        app.tables.staticTexts["Контакты"].tap()
+        app.staticTexts["Телеграмм канал"].tap()
+    }
+    
+    func testSMEDarkContact() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        app.buttons["SME Dark"].tap()
+        app.buttons["InfoTabBarItem"].tap()
+        app.tables.staticTexts["Контакты"].tap()
+        app.staticTexts["Телеграмм канал"].tap()
+    }
+    
+    func testSMELightContact() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        app.buttons["SME Light"].tap()
+        app.buttons["InfoTabBarItem"].tap()
+        app.tables.staticTexts["Контакты"].tap()
+        app.staticTexts["Телеграмм канал"].tap()
+    }
+}
---
+++ ExampleiOS/Tests/AdmiralSwiftUITests/AdmiralSwiftUIInfoTest.swift
@@ -0,0 +1,88 @@
+//
+//  AdmiralSwiftUIInfoTest.swift
+//  AdmiralSwiftUITests
+//
+
+import XCTest
+
+class AdmiralSwiftUIInfoTest: XCTestCase {
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+    
+    override func tearDownWithError() throws {
+    }
+    
+    func testCheckSwiftUI() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["SeatingTabBarItem"].tap()
+        let swiftUISwitcher = app.switches["SwitchToSwiftUI"]
+        
+        if swiftUISwitcher.value as? String == "0" {
+            swiftUISwitcher.tap()
+        }
+    }
+    
+    func testInfoDark() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["Dark"].tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["InfoTabBarItem"].tap()
+        app.tables.staticTexts["Инфо"].tap()
+        sleep(2)
+    }
+    
+    func testInfoLight() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["Light"].tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["InfoTabBarItem"].tap()
+        app.tables.staticTexts["Инфо"].tap()
+        sleep(2)
+    }
+    
+    func testInfoSMEDark() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["SME Dark"].tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["InfoTabBarItem"].tap()
+        app.tables.staticTexts["Инфо"].tap()
+        sleep(2)
+    }
+    
+    func testInfoSMELight() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["SME Light"].tap()
+        
+        app.buttons["Ellipse"].tap()
+        
+        app.buttons["InfoTabBarItem"].tap()
+        app.tables.staticTexts["Инфо"].tap()
+        sleep(2)
+    }
+}
