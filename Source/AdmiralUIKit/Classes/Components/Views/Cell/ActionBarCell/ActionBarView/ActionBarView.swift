5fecb03177fabc5e4582dcf767750cf939a3b6ff
Алмазов Иван Александрович <meaplasma@gmail.com>
2023-03-01 18:56:08 +0300

    Some fix

--- Source/AdmiralUIKit/Classes/Components/Views/Cell/ActionBarCell/ActionBarView/ActionBarView.swift
+++ Source/AdmiralUIKit/Classes/Components/Views/Cell/ActionBarCell/ActionBarView/ActionBarView.swift
@@ -18,15 +18,15 @@ private protocol ActionBarViewStyleProtocol {
 }
 
 class ActionBarView: UIView, ActionBarViewStyleProtocol, AnyAppThemable {
-
+    
     enum Constant {
         static let width: CGFloat = 32.0
         static let buttonInset: CGFloat = LayoutGrid.halfModule * 3
         static let customWidth: CGFloat = 90
     }
-
+    
     // MARK: - Internal Properties
-
+    
     var isEnabled: Bool {
         get { !state.contains(.disabled) }
         set {
@@ -37,87 +37,91 @@ class ActionBarView: UIView, ActionBarViewStyleProtocol, AnyAppThemable {
             }
         }
     }
-
+    
     /// The state of the view. Default is normal.
     var state: UIControl.State = .normal {
         didSet { configure(for: state) }
     }
-
+    
     var scheme = ActionBarViewScheme() {
         didSet { updateScheme() }
     }
-
+    
     init(style: ActionBarViewStyle = .default) {
         self.style = style
         super.init(frame: .zero)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: - Computed properties
-
+    
     private var width: CGFloat {
         style == .default ? Constant.width : Constant.customWidth
     }
-
+    
     // MARK: - AnyAppThemable
-
+    
     func apply(theme: AppTheme) {
         scheme = ActionBarViewScheme(theme: theme)
-        views.forEach({
-            ($0 as? (any AnyAppThemable))?.apply(theme: theme)
-        })
+        views.forEach {
+            if let view = $0 as? ActionItemBarDefaultView {
+                view.apply(theme: theme)
+            } else if let view = $0 as? ActionItemBarViewSecondary {
+                view.apply(theme: theme)
+            }
+        }
     }
-
+    
     // MARK: - Private Properties
-
+    
     private let style: ActionBarViewStyle
     private var views: [ActionBarViewPresentable] = []
-
+    
     // MARK: - Internal Methods
-
+    
     func appendAction(_ action: ActionItemBarAction) {
         let actionBarView = configureView(action)
         configureTralingViewConstraint(actionBarView, index: views.count)
-
+        
         views.append(actionBarView)
     }
-
+    
     func insertAction(_ action: ActionItemBarAction, index: Int) {
         guard views.count > index else {
             appendAction(action)
             return
         }
-
+        
         let actionBarView = configureView(action)
         views.insert(actionBarView, at: index)
-
+        
         for index in 0..<views.count {
             configureTralingViewConstraint(views[index], index: index)
         }
     }
-
+    
     func removeAction(from index: Int) {
         views.remove(at: index)
-
+        
         for index in 0..<views.count {
             configureTralingViewConstraint(views[index], index: index)
         }
     }
-
+    
     // MARK: - Private Methods
-
+    
     private func configure(for state: UIControl.State) {
         for view in views {
             view.isEnabled = isEnabled
         }
     }
-
+    
     private func configureTralingViewConstraint(_ view: ActionBarViewPresentable, index: Int) {
         var offsetTraling: CGFloat = .zero
-
+        
         if index == 0 {
             offsetTraling = style == .default ? LayoutGrid.doubleModule : .zero
         } else {
@@ -125,7 +129,7 @@ class ActionBarView: UIView, ActionBarViewStyleProtocol, AnyAppThemable {
             let customTrailing: CGFloat = width * CGFloat(index)
             offsetTraling = style == .default ? defaultTrailing : customTrailing
         }
-
+        
         if view.tralingConstraint != nil {
             view.tralingConstraint?.constant = offsetTraling
         } else {
@@ -136,22 +140,22 @@ class ActionBarView: UIView, ActionBarViewStyleProtocol, AnyAppThemable {
             view.tralingConstraint = tralingAnchor
         }
     }
-
+    
     private func configureView(_ action: ActionItemBarAction) -> ActionBarViewPresentable {
         let view = build(by: action)
         view.translatesAutoresizingMaskIntoConstraints = false
         addSubview(view)
-
+        
         NSLayoutConstraint.activate([
             view.topAnchor.constraint(equalTo: topAnchor),
             view.bottomAnchor.constraint(equalTo: bottomAnchor),
             view.widthAnchor.constraint(equalToConstant: width),
             view.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor, constant: style == .default ? LayoutGrid.doubleModule : .zero)
         ])
-
+        
         return view
     }
-
+    
     private func updateScheme() {
         backgroundColor = scheme.backgroundColor.uiColor
         views.forEach {
@@ -162,12 +166,12 @@ class ActionBarView: UIView, ActionBarViewStyleProtocol, AnyAppThemable {
             }
         }
     }
-
+    
 }
 
 // MARK: - Protocol extensions
 extension ActionBarViewStyleProtocol {
-
+    
     func build(by action: ActionItemBarAction) -> ActionBarViewPresentable {
         switch action.style {
         case .default:
@@ -176,5 +180,5 @@ extension ActionBarViewStyleProtocol {
             return ActionItemBarViewSecondary(action: action)
         }
     }
-
+    
 }
