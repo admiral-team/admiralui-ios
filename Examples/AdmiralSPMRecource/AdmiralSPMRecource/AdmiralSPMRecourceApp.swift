9df4a1476f744059be6621c7f0f13a7a06b5f8f1
Andyatkov <andyatkov@gmail.com>
2023-03-31 11:38:16 +0300

    [767] - Add example project for binary

---
+++ Examples/AdmiralSPMRecource/AdmiralSPMRecource/AdmiralSPMRecourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmiralSPMRecourceApp.swift
+//  AdmiralSPMRecource
+//
+//  Created by ADyatkov on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AdmiralSPMRecourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
