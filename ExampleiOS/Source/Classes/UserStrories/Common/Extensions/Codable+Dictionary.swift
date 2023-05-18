5379c1547b6a4acb65e9e64d1e128bd3d0666921
ton252 <tonwork252@gmail.com>
2023-05-18 15:24:41 +0300

    Fix warnings and errors

--- ExampleiOS/Source/Classes/UserStrories/Common/Extensions/Codable+Dictionary.swift
+++ ExampleiOS/Source/Classes/UserStrories/Common/Extensions/Codable+Dictionary.swift
@@ -12,7 +12,7 @@ extension Encodable {
     func asDictionary() throws -> [String: Any] {
         let data = try JSONEncoder().encode(self)
         guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
-            throw NSError()
+            throw NSError(domain: "unknown", code: 0)
         }
         return dictionary
     }
