888a55be211d7c982888838e2417703063bc7301
Andyatkov <andyatkov@gmail.com>
2023-05-25 15:19:13 +0300

    Fix checksumma

--- fastlane/checksum.rb
+++ fastlane/checksum.rb
@@ -33,12 +33,11 @@ def admiral_symbols
 end
   
 
-def set_checksum(admiral_swiftui_checksum:, admiral_theme_checksum:, admiral_resources_checksum:, admiral_uikit_checksum:, admiral_symbols_checksum:, admiral_images_checksum:)
+def set_checksum(admiral_swiftui_checksum:, admiral_theme_checksum:, admiral_uikit_checksum:, admiral_symbols_checksum:, admiral_images_checksum:)
   file_name = ENV['CHECKSUM_FILE']
   params = JSON.parse(File.read(file_name))
   params['AdmiralSwiftUI'] = admiral_swiftui_checksum if !admiral_swiftui_checksum.nil?
   params['AdmiralTheme'] = admiral_theme_checksum if !admiral_theme_checksum.nil?
-  params['AdmiralUIResources'] = admiral_resources_checksum if !admiral_resources_checksum.nil?
   params['AdmiralUIKit'] = admiral_uikit_checksum if !admiral_uikit_checksum.nil?
   params['AdmiralImages'] = admiral_images_checksum if !admiral_images_checksum.nil?
   params['AdmiralSymbols'] = admiral_symbols_checksum if !admiral_symbols_checksum.nil?
